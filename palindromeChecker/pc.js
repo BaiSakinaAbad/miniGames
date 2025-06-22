033164783ab9229f22cc834c57c1ea9f25a555bd
Bai Sakina Abad <kimireneabad07@gmail.com>
2025-06-23 01:51:08 +0800

    major changes for palindrome checker

--- palindromeChecker/pc.js
+++ palindromeChecker/pc.js
@@ -1,17 +1,28 @@
-// getting the id using getElementById
-const input =  document.getElementById("text-input");
+const input = document.getElementById("text-input");
 const checkBtn = document.getElementById("check-btn");
 const result = document.getElementById("result");
 
-
 checkBtn.addEventListener("click", () => {
-    const inputWord = input.value;
-    
-    if (inputWord.trim() === "") {
+  const inputWord = input.value;
+  
+  if (inputWord.trim() === "") {
+    // alert if no input
     alert("Please input a value.");
     return;
-  } else{
-    // code for trim, lowercase, reverse and checking
+  } else {
+    // clean the input
+    const cleanStr = inputWord.trim().toLowerCase();
+    // remove all spaces and non-alphabetical characters
+    const noSpace = inputWord.toLowerCase().replace(/[^a-z0-9]/g, "");
+    // reverse is a method for array, not string, so we convert the string into an array, reverse that array, and then join it into a string
+    const reversed = noSpace.split('').reverse().join('');
+    
+    if (noSpace === reversed) {
+      result.innerText = `"${inputWord}" is a Palindrome`;
+      //result.style.color = "green";
+    } else {
+      result.innerText = `"${inputWord}" is not a Palindrome`;
+     // result.style.color = "red";
+    }
   }
-
 });
